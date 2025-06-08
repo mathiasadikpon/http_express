490dc3e755cca6792c19faa38592ee1a9f1d63e2
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-06-08 16:50:42 -0400

    express.static and morgan use

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,17 @@
+// requires
 const express = require("express");
+const morgan = require("morgan");
 
 const port = 3000;
 const hostName = "localhost";
 
-const app = expresss();
+const app = express();
+app.use(morgan("dev")); // Logging middleware console logs
+
+app.use(express.static(__dirname + "/public")); // Serve static files from the public directory
 
 app.use((req, res) => {
-  console.log(req.headers);
+  //console.log(req.headers); // Morgan take care of it
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>Hello from Express Server</h1></body></html>");
