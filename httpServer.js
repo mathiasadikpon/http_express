e0981e2edadca90f154865793a22362cea27bc1f
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-06-07 23:32:17 -0400

    add fs and path

--- httpServer.js
+++ httpServer.js
@@ -1,14 +1,45 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const port = 3000;
 const hostName = "localhost";
 
 const server = http.createServer((req, res) => {
-  console.log("Server received a request");
-  res.setHeader("Content-Type", "text/html");
-  res.statusCode = 200;
-
-  res.end("<html><body><h1>Hello, World!</h1></body></html>");
+  let fileUrl = req.url;
+  if (fileUrl === "/") {
+    fileUrl = "/index.html";
+  }
+  const filePath = path.resolve("./public" + fileUrl);
+  const fileExt = path.extname(filePath);
+  if (fileExt !== ".html") {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<html><body><h1>Error 404: Not a HTML file</h1></body></html>");
+    return;
+  }
+  fs.access(filePath, (err) => {
+    if (err) {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      res.end("<html><body><h1>Error 404: File not found</h1></body></html>");
+      return;
+    }
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "text/html");
+        res.end(
+          "<html><body><h1>Error 500: Internal Server Error</h1></body></html>"
+        );
+        return;
+      }
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
+    });
+  });
+  console.log(`Request for ${fileUrl} received.`);
 });
 
 server.listen(port, hostName, () => {
